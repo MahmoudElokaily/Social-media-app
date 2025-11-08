c8cd26decfe77ed6af7b892db0920124df7c1ed1
Mahmoud El-Okaily <mahmoudelokaily3@gmail.com>
2025-11-08 13:23:40 +0200

    Health check

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,11 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  health() {
+    return {
+      status: 'ok',
+      message: 'Server is healthy',
+      timestamp: new Date().toISOString(),
+    };
   }
 }
